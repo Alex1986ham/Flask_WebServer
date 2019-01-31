11b8bcc2e9c51286224a876bd2584516ec826a4a
Dudko <ALDUDKO@baumarktdirekt.local>
2019-01-31 16:52:17 +0100

    Refering to rendered templates

--- Flask/__init__.py
+++ Flask/__init__.py
@@ -1,7 +1,56 @@
-from flask import Flask
+# Do that in terminal
+    # pip install flask
+    # from flask import Flask, render_template
+    # mysql -u <username> -import pdb; pdb.set_trace()
+    # CREATE DATABASE BucketList;
+    # CREATE TABLE 'BucketList'.'tbl_user' (
+    #   'user_id' BIGINT Null AUTO_INCREMENT,
+    #   'user_name' VARCHAR(45) Null,
+    #   'user_password' VARCHAR(45) Null,
+    #   PRIMARY KEY ('user_id'));
+    )
+
+from flask import Flask; render_template
 app = Flask(__name__)
+
 @app.route("/")
-def hello():
-    return "Welcome to my first deployed web server based on python, mysql, php. This is just the beginning of somthing reallllllllyyyyy BIG. :-) --> Created by Shurik<--"
+def main():
+    return render_template('index.html')
+
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+
+# Begin Check if user exists
+
+DELIMITER $$
+CREATE DEFINER='root'@'localhost' PROCEDURE 'sp_createUser'(
+    IN p_name VARCHAR(20),
+    IN p_username VARCHAR(20),
+    IN p_password VARCJHAR(20)
+)
+BEGIN
+    if ( select exists (select 1 from tbl_user where user_username = p_username)) THEN
+
+        select 'Username Exists !!';
+
+    ELSE
+
+        insert into tbl_user
+        (
+            user_name,
+            user_username,
+            user_password
+        );
+
+    END If;
+END$$
+DELIMITER ;
+
+# End check if user exists
+
+
 if __name__ == "__main__":
     app.run()
